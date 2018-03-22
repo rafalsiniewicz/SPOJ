27275887b88beb373219c1f8fc4d835928f16351
rafalsiniewicz <36672426+rafalsiniewicz@users.noreply.github.com>
2018-03-22 20:24:53 +0100

    Update 723.cpp

--- 723.cpp
+++ 723.cpp
@@ -1,3 +1,10 @@
+/*Przesuń elementy tablicy cyklicznie w lewo.
+
+Wejście
+Najpierw liczba testów t (t ≤ 100). Następnie dla każdego testu liczba n (1 < n ≤ 100) i n liczb.
+
+Wyjście
+Dla każdego testu n liczb w zmienionym porządku.*/
 #include<iostream>
 using namespace std;
 int* shift(int tab2[], int s)
