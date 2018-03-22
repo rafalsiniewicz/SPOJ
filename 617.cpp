bb9921cfeaa8e674032c2e7589e284ac99c92622
rafalsiniewicz <36672426+rafalsiniewicz@users.noreply.github.com>
2018-03-22 20:17:52 +0100

    Update 617.cpp

--- 617.cpp
+++ 617.cpp
@@ -1,3 +1,17 @@
+/*Napisz funkcję:
+
+char* string_merge(char *, char *);
+która sklei ze sobą dwa łańcuchy biorąc na przemian po jednym znaku z każdego łańcucha i 
+umieści w nowej dynamicznie alokowanej tablicy znaków, do której zwróci wskaźnik. 
+Należy wziąć po tyle znaków ile jest w krótszym łańcuchu.
+
+Input
+ 
+
+W pierwszej linii liczba testów t, w kolejnych liniach po dwa łańcuchy znaków odzielone spacją.
+
+Output
+W każdej linii jeden łańcuch, wynik działania funkcji string_merge.*/
 #include<iostream>
 using namespace std;
 char* string_merge(char* s1, char* s2, int a, int b){
