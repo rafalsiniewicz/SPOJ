92059d732feae48a4374c633a2e14457fd3d2379
rafalsiniewicz <36672426+rafalsiniewicz@users.noreply.github.com>
2018-03-22 10:56:39 +0100

    Update 499.cpp

--- 499.cpp
+++ 499.cpp
@@ -1,3 +1,16 @@
+/*Dla danych dwóch liczb naturalnych a i b, wyznaczyć ostatnią cyfrę liczby ab.
+
+Zadanie
+Napisz program, który:
+wczyta ze standardowego wejścia: podstawę a oraz wykładnik b,
+wyznaczy ostatnią cyfrę liczby a^b,
+wypisze wynik na standardowe wyjście.
+Wejście
+W pierwszej linii wejścia znajduje się jedna liczba całkowia D (1≤D≤10), oznaczjąca liczbę przypadków do rozważenia. Opis każdego przypadku podany jest w jednym wierszu, zawierającym dwie liczby naturalne a i b oddzielone pojedynczym odstępem (spacją), takie, że (1 ≤ a,b ≤ 1 000 000 000).
+
+Wyjście
+Dla każdego przypadku z wejścia Twój program powinien wypisać (w osobnej linii dla każdego przypadku z wejścia) cyfrę jedności liczby ab zapisanej dziesiętnie.
+*/
 #include<iostream>
 #include<cmath>
 #include<string>
