cedb69515ca88ccbd040279b204f48ff74297996
rafalsiniewicz <36672426+rafalsiniewicz@users.noreply.github.com>
2018-03-22 20:16:22 +0100

    Update 606.cpp

--- 606.cpp
+++ 606.cpp
@@ -1,3 +1,10 @@
+/*Odwróć kolejność elementów w tablicy.
+
+Wejście
+Najpierw liczba testów t (t ≤ 100). Następnie dla każdego testu liczba n (n ≤ 100) i n liczb oddzielonych spacjami.
+
+Wyjście
+Dla każdego testu n liczb w porządku odwrotnym niż na wejściu.*/
 #include<iostream>
 using namespace std;
 int* rev(int* tab, int n){
