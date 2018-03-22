b0b4a58ea380aa7929199361519dc06ed1cd722b
rafalsiniewicz <36672426+rafalsiniewicz@users.noreply.github.com>
2018-03-22 20:24:31 +0100

    Update 708.cpp

--- 708.cpp
+++ 708.cpp
@@ -1,3 +1,17 @@
+/*Dany jest ciąg xn określony rekurencyjnie:
+x0=s, 
+xn+1=3*xn+1, jeśli xn jest nieparzyste i 
+xn+1=xn/2, jeśli xn jest parzyste
+
+Napisz program, który oblicza pierwsze takie n, dla którego xn=1.
+
+Wejście
+ 
+
+W pierwszej linii liczba testów t. W każdym z t kolejnych wierszy jedna liczba całkowita s, 1 <= s <= 10000.
+
+Wyjście
+W każdej linii jedna liczba - obliczona wartość n.*/
 #include<iostream>
 using namespace std;
 int x = 0;
